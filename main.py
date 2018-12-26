b32f93bb37828959cbb5b4abe84fffce29ca170c
Ivan Lozgachev <lozgachev.ivan@gmail.com>
2018-12-26 14:05:57 +0300

    Small update

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_login.login_manager import LoginManager
+from flask_login.utils import login_user
+from auth import get_user
 
 app = Flask(__name__)
+login_manager = LoginManager()
+app.config['SECRET_KEY'] = "secret_key"
+login_manager.init_app(app)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = get_user(username)
+        if user.password == password:
+            login_user(user)
+            return render_template("home.html")
+        else:
+            return render_template("login.html", fail_auth=True)
+    else:
+        return render_template("login.html", fail_auth=False)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
